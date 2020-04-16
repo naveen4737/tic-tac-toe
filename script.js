b84a44ff471523a3a80ff91e16cc7d49d9071938
naveen4737 <naveendongre4737@gmail.com>
2020-04-17 01:16:28 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,144 @@
+var current_player = 'Player1';
+var gameStopped = false;
+document.getElementById('player1').style.backgroundColor = 'red';
+
+
+// console.log(document.getElementById('box').addEventListener('click', print(this)));
+console.log('gcgvcbhjn')
+// document.getElementById('box').addEventListener('onclick', print(this))
+
+var locations = document.getElementsByClassName('inter');
+console.log(locations)
+ 
+for(var i=0; i<locations.length; i++){
+    console.log(locations[i].innerHTML, " : 789645")
+    locations[i].addEventListener('click', print);
+    locations[i].addEventListener('mouseover', hovering);
+    locations[i].addEventListener('mouseout', nothovering);
+    //  console.log(locations[i]) 
+}
+
+function hovering(){
+    // console.log(this,'   in HOVER')
+    if(this.innerHTML == ''){
+        this.style.backgroundColor = 'wheat';
+    }
+}
+
+function nothovering(){
+    // console.log(this,'   out HOVER')
+    this.style.backgroundColor = 'brown';
+}
+ 
+function print(){
+
+    var current_location = this.id;
+    if( gameStopped == false && document.getElementById(current_location).textContent == ''){
+
+        if(current_player === 'Player1'){
+            document.getElementById(current_location).textContent = 'X';
+            current_player = 'Player2';
+            document.getElementById('player1').style.backgroundColor = 'white';
+            document.getElementById('player2').style.backgroundColor = 'red';
+        } else if(current_player === 'Player2'){
+            document.getElementById(current_location).textContent = 'O';
+            current_player = 'Player1';
+            document.getElementById('player1').style.backgroundColor = 'red';
+            document.getElementById('player2').style.backgroundColor = 'white';
+        }
+
+        for(var i=0; i<3 ; i++){
+            
+            var xr1 = 0;
+            var or1 = 0;
+
+            var xc1 = 0;
+            var oc1 = 0;
+            for(var j=0; j<3 ; j++){
+                // console.log('i and j are, ', i, '  ', j)
+                var contentRow = document.getElementById(`${i}${j}`).innerHTML;
+                var contentColumn = document.getElementById(`${j}${i}`).innerHTML;
+
+                // console.log(content)
+
+                if( contentRow == 'X' ){
+                    xr1++;
+                } else if( contentRow == 'O' ){
+                    or1++;
+                }
+                if( xr1 == 3){
+                    winner('Player 1');
+                    gameStopped = true;
+                } else if( or1 == 3){
+                    winner('Player 2');
+                    gameStopped = true;
+                }
+
+                if( contentColumn == 'X' ){
+                    xc1++;
+                } else if( contentColumn == 'O' ){
+                    oc1++;
+                }
+                if( xc1 == 3){
+                    winner('Player 1');
+                    gameStopped = true;
+                } else if( oc1 == 3){
+                    winner('Player 2');
+                    gameStopped = true;
+                }
+            }
+        }
+
+        
+        var xl1 = 0;
+        var ol1 = 0;
+
+        var xr1 = 0;
+        var or1 = 0;
+        for(var i=0; i<3 ; i++){
+            
+            for(var j=0; j<3 ; j++){
+
+                if( i == j){
+                    var contentLeft = document.getElementById(`${i}${j}`).innerHTML;
+                    if( contentLeft == 'X' ){
+                        xl1++;
+                    } else if( contentLeft == 'O' ){
+                        ol1++;
+                    }
+                    if( xl1 == 3){
+                        winner('Player 1');
+                        gameStopped = true;
+                    } else if( ol1 == 3){
+                        winner('Player 2');
+                        gameStopped = true;
+                    }
+                }
+                if(i+j == 2){
+                    var contentRight = document.getElementById(`${i}${j}`).innerHTML;
+                    if( contentRight == 'X' ){
+                        xr1++;
+                    } else if( contentRight == 'O' ){
+                        or1++;
+                    }
+                    if( xr1 == 3){
+                        winner('Player 1');
+                        gameStopped = true;
+                    } else if( or1 == 3){
+                        winner('Player 2');
+                        gameStopped = true;
+                    }
+                }
+            }
+        }
+    
+    }
+}
+
+function winner(text){
+    console.log('Winner is ', text);
+    document.getElementById('announcement').innerHTML = `${text} WON THE GAME!!!`;
+
+    document.getElementById('player1').style.backgroundColor = 'yellow';
+    document.getElementById('player2').style.backgroundColor = 'yellow';
+}
